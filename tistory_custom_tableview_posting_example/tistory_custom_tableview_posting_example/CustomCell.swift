b2b3273a7f15c99463dfca3d5c7ba781a0275a7e
ehdrjsdlzzzz <ehdrjsdlzzzz@gmail.com>
2017-11-01 01:04:30 +0900

    view_change_data_pass_sample1 - ui setting

--- tistory_custom_tableview_posting_example/tistory_custom_tableview_posting_example/CustomCell.swift
+++ tistory_custom_tableview_posting_example/tistory_custom_tableview_posting_example/CustomCell.swift
@@ -9,22 +9,7 @@
 import UIKit
 
 class CustomCell: UITableViewCell {
-
-    
     @IBOutlet weak var weatherImgView: UIImageView!
     @IBOutlet weak var areaLabel: UILabel!
     @IBOutlet weak var weatherLabel: UILabel!
-    
-    
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
 }
---
+++ view_change_data_pass_sample1/view_change_data_pass_sample1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  view_change_data_pass_sample1
+//
+//  Created by 이동건 on 2017. 11. 1..
+//  Copyright © 2017년 이동건. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
