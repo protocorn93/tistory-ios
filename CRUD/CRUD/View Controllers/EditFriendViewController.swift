0ceba49f86f838a7291e283acd005676d2bcc5a0
ehdrjsdlzzzz <ehdrjsdlzzzz@gmail.com>
2017-12-22 16:00:00 +0900

    UserDefault/PropertyListEncoder

--- CRUD/CRUD/View Controllers/EditFriendViewController.swift
+++ CRUD/CRUD/View Controllers/EditFriendViewController.swift
@@ -27,7 +27,7 @@ class EditFriendViewController: UIViewController {
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
