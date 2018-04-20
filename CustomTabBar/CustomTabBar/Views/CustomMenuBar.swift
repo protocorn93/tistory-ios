d2dff4476ab8ed1a4464c8b3997b118ddb7fe721
ehdrjsdlzzzz <ehdrjsdlzzzz@gmail.com>
2018-04-20 15:18:25 +0900

    customMenuBar View

---
+++ CustomTabBar/CustomTabBar/Views/CustomMenuBar.swift
@@ -0,0 +1,114 @@
+//
+//  CustomMenuBar.swift
+//  CustomTabBar
+//
+//  Created by 이동건 on 2018. 4. 20..
+//  Copyright © 2018년 이동건. All rights reserved.
+//
+
+import UIKit
+
+protocol CustomMenuBarDelegate: class {
+    func customMenuBar(to index: Int)
+}
+
+class CustomMenuBar: UIView {
+    
+    weak var delegate: CustomMenuBarDelegate?
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.backgroundColor = .white
+        setupCustomTabBar()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    var customTabBarCollectionView: UICollectionView = {
+        let collectionViewLayout = UICollectionViewFlowLayout()
+        collectionViewLayout.scrollDirection = .horizontal
+        let collectionView = UICollectionView(frame: CGRect(x: 0, y: 0, width: 0, height: 0), collectionViewLayout: collectionViewLayout)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.backgroundColor = .white
+        return collectionView
+    }()
+    
+    var indicatorView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .black
+        return view
+    }()
+    //MARK: Properties
+    var indicatorViewLeadingConstraint:NSLayoutConstraint!
+    var indicatorViewWidthConstraint: NSLayoutConstraint!
+    //MARK: Setup Views
+    func setupCollectioView(){
+        customTabBarCollectionView.delegate = self
+        customTabBarCollectionView.dataSource = self
+        customTabBarCollectionView.showsHorizontalScrollIndicator = false
+        customTabBarCollectionView.register(UINib(nibName: CustomCell.reusableIdentifier, bundle: nil), forCellWithReuseIdentifier: CustomCell.reusableIdentifier)
+        customTabBarCollectionView.isScrollEnabled = false
+        
+        let indexPath = IndexPath(item: 0, section: 0)
+        customTabBarCollectionView.selectItem(at: indexPath, animated: false, scrollPosition: [])
+    }
+    
+    func setupCustomTabBar(){
+        setupCollectioView()
+        self.addSubview(customTabBarCollectionView)
+        customTabBarCollectionView.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
+        customTabBarCollectionView.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
+        customTabBarCollectionView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        customTabBarCollectionView.heightAnchor.constraint(equalToConstant: 55).isActive = true
+        
+        self.addSubview(indicatorView)
+        indicatorViewWidthConstraint = indicatorView.widthAnchor.constraint(equalToConstant: self.frame.width / 4)
+        indicatorViewWidthConstraint.isActive = true
+        indicatorView.heightAnchor.constraint(equalToConstant: 5).isActive = true
+        indicatorViewLeadingConstraint = indicatorView.leadingAnchor.constraint(equalTo: self.leadingAnchor)
+        indicatorViewLeadingConstraint.isActive = true
+        indicatorView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+    }
+    
+}
+
+//MARK:- UICollectionViewDelegate, DataSource
+extension CustomMenuBar: UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CustomCell.reusableIdentifier, for: indexPath) as! CustomCell
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: self.frame.width / 4 , height: 55)
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        delegate?.customMenuBar(to: indexPath.row)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
+        guard let cell = collectionView.cellForItem(at: indexPath) as? CustomCell else {return}
+        cell.label.textColor = .lightGray
+    }
+}
+//MARK:- UICollectionViewDelegateFlowLayout
+extension CustomMenuBar: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+}
+
