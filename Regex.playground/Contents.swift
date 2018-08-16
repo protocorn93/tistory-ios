7fa883a1868966e6bef3fac260fe00cd2443cfe4
ehdrjsdlzzzz <ehdrjsdlzzzz@gmail.com>
2018-08-16 23:30:03 +0900

    Regex. 수정

--- Regex.playground/Contents.swift
+++ Regex.playground/Contents.swift
@@ -1,76 +1,87 @@
 //: Playground - noun: a place where people can play
 
 import Cocoa
-
-var rawValue = "{asdf:asdf}, {asfd}, {sss[]} [asdf]"
-
-var invalidValue1 = "{ \"name\" : \"KIM JUNG\", \"alias\" : \"JK\", \"level\" : 5, \"children\" : [\"hana\", \"hayul\", \"haun\"] }"
-var invalidValue2 = "[ \"name\" : \"KIM JUNG\" ]"
-var validValue1 = "{ \"name\" : \"KIM JUNG\", \"alias\" : \"JK\", \"level\" : 5, \"married\" : true }"
-var validValue2 = "[ { \"name\" : \"KIM JUNG\", \"alias\" : \"JK\", \"level\" : 5, \"married\" : true }, { \"name\" : \"YOON JISU\", \"alias\" : \"crong\", \"level\" : 4, \"married\" : true } ]"
 /*
  .caseInsensitive : both uppercase and lowecase
 */
+let stringPattern = "^\"(?!.*\"\\s*:\\s*).*\"$"
+let integerPattern = "^[0-9]+$"
+let booleanPattern = "(^false|^true)"
 
-extension String {
-    var isValid: Bool {
-        return !isNested && !isPairInArray
-    }
-    
-    private var isNested: Bool {
-        if let regex = try? NSRegularExpression(pattern: "\\{(?:.+?)?(?:\\{|\\[)(?:.+?)?(?:\\}|\\])(?:.+?)?\\}", options: .caseInsensitive) {
-            let string = self as NSString
-            return !regex.matches(in: self, options: [], range: NSRange(location: 0, length: string.length)).isEmpty
+let valuesPattern = "true|false|[0-9]+|\".*\""
+let singleArrayPattern = "\\[\\s*(\(valuesPattern))(,\\s*(\(valuesPattern)))*\\s*\\]"
+let singleObjectPattern = "\\{\\s*\".*\"\\s*:\\s*(\(valuesPattern))\\s*(,\\s*\".*\"\\s*:\\s*(\(valuesPattern)))*\\s*\\}"
+
+//let nestedObjectPattern = "^\\{\\s*\".*\"\\s*:\\s*(\(singleObjectPattern)|\(singleArrayPattern)|\(valuesPattern))\\s*(,\\s*\".*\"\\s*:\\s*(\(singleObjectPattern)|\(singleArrayPattern)|\(valuesPattern))\\s*)*\\}$"
+let nestedObjectPattern = "^\\{\\s*\".*\"\\s*:\\s*(\(valuesPattern)|\(singleArrayPattern)|\(singleObjectPattern))\\s*(,(\(valuesPattern)|\(singleArrayPattern)|\(singleObjectPattern)))*\\s*\\}$"
+let nestedArrayPattern = "^\\[\\s*(\(valuesPattern)|(\(singleObjectPattern)|\(singleArrayPattern)))\\s*(,\\s*(\(valuesPattern)|(\(singleObjectPattern)|\(singleArrayPattern))))*\\s*\\]$"
+
+
+func isSignleObject(target: String) -> [String] {
+    let value = target as NSString
+    if let regex = try? NSRegularExpression(pattern: singleObjectPattern, options: .caseInsensitive) {
+        return regex.matches(in: target, options: [], range: NSRange(location: 0, length: value.length)).map {
+            value.substring(with: $0.range)
         }
-        return false
     }
-    
-    private var isPairInArray: Bool {
-        if let regex = try? NSRegularExpression(pattern: "\\[[a-z0-9\"\\s]+:[a-z0-9\"\\s]+\\]", options: .caseInsensitive) {
-            let string = self as NSString
-            return !regex.matches(in: self, options: [], range: NSRange(location: 0, length: string.length)).isEmpty
-        }
-        
-        return false
-    }
+    return []
 }
 
-struct JSONString {
-    private var target: String
-    
-    init(target: String) {
-        self.target = target
-    }
-    var keys: [String] {
-        if let regex = try? NSRegularExpression(pattern: "\"[a-z]+\"\\s?(?=:)", options: .caseInsensitive) {
-            let string = target as NSString
-            return regex.matches(in: target, options: [], range: NSRange(location: 0, length: string.length)).map {
-                string.substring(with: $0.range).replacingOccurrences(of: ":", with: "")
-            }
+
+
+func isObject(target: String) -> [String] {
+    let value = target as NSString
+    if let regex = try? NSRegularExpression(pattern: nestedObjectPattern, options: .caseInsensitive) {
+        return regex.matches(in: target, options: [], range: NSRange(location: 0, length: value.length)).map {
+            value.substring(with: $0.range)
         }
-        return []
     }
-    var values: [String] {
-        if let regex = try? NSRegularExpression(pattern: "[^:]+(?=,|$)", options: .caseInsensitive) {
-            let string = target as NSString
-            return regex.matches(in: target, options: [], range: NSRange(location: 0, length: string.length)).map {
-                string.substring(with: $0.range).replacingOccurrences(of: ":", with: "")
-            }
-        }
-        return []
-    }
-    var objects: [String] {
-        if let regex = try? NSRegularExpression(pattern: "[^\\{]+(?=\\})", options: .caseInsensitive) {
-            let string = target as NSString
-            return regex.matches(in: target, options: [], range: NSRange(location: 0, length: string.length)).map {
-                string.substring(with: $0.range).replacingOccurrences(of: ":", with: "")
-            }
-        }
-        return []
-    }
+    return []
 }
-let objects = JSONString(target: "{asdf}, {asdf}").objects
-let values = JSONString(target: "\"name\": \"\"South Korea, United Kingdom\"").values
-let keys = JSONString(target: "\"name\" : \"\"South Korea, United Kingdom\"").keys
-values.forEach {print($0)}
-keys.forEach {print($0)}
+
+func isSingleArray(target: String) -> [String] {
+    let value = target as NSString
+    if let regex = try? NSRegularExpression(pattern: singleArrayPattern, options: .caseInsensitive) {
+        return regex.matches(in: target, options: [], range: NSRange(location: 0, length: value.length)).map {
+            value.substring(with: $0.range)
+        }
+    }
+    return []
+}
+
+func isArray(target: String) -> [String] {
+    let value = target as NSString
+    if let regex = try? NSRegularExpression(pattern: nestedArrayPattern, options: .caseInsensitive) {
+        return regex.matches(in: target, options: [], range: NSRange(location: 0, length: value.length)).map {
+            value.substring(with: $0.range)
+        }
+    }
+    return []
+}
+
+let arrayText1 = "[\"key\", false, 5]"
+print("------Single Array------")
+isSingleArray(target: arrayText1).forEach {print($0)}
+print("------------------------")
+print("------Single Object------")
+let objectText1 = "{ \"name\" : \"KIM JUNG\", \"alias\" : \"JK\", \"level\" : [5], \"married\" : true }"
+isSignleObject(target: objectText1).forEach {print($0)}
+print("------------------------")
+print("------Array Nested Object------")
+let objectWithArrayText = "{ \"name\" : \"KIM JUNG\", \"child\" : [5, \"b\", true], \"alias\" : \"JK\", \"level\" : 5, \"married\" : true }"
+isObject(target: objectWithArrayText).forEach {print($0)}
+print("------------------------")
+print("------Object Nested Object------")
+let objectWithObjectText = "{ \"name\" : \"KIM JUNG\", \"child\" : {\"married\" : true}, \"alias\" : \"JK\", \"level\" : 5, \"married\" : true }"
+isObject(target: objectWithObjectText).forEach {print($0)}
+print("------------------------")
+print("------Object Nested Array------")
+let arrayWithObjectText = "[\"false\", 5, false, {\"key\":5}]"
+isArray(target: arrayWithObjectText).forEach {print($0)}
+print("------------------------")
+print("------Array Nested Array------")
+let arrayWithArrayText = "[\"false\", 5, false, [false, 5, \"asd\"]]"
+isArray(target: arrayWithArrayText).forEach {print($0)}
+print("------------------------")
+print("------Double Nested Object------")
+isObject(target: "{ \"name\" : \"KIM JUNG\", \"child\" : {\"age\": [5,4,2] }, \"alias\" : \"JK\", \"level\" : 5, \"married\" : true }").forEach {print($0)}
